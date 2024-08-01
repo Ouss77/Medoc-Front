318aac345ad37aea3dc6c254846f0029b46d48e8
Ouss77 <oussamasassour24@gmail.com>
2024-08-01 17:16:51 +0100

    v.1

--- back-end/server.js
+++ back-end/server.js
@@ -3,28 +3,39 @@ const mongoose = require('mongoose');
 const userRoutes = require('./UserRoutes');
 const visitRoutes = require('./VisitRoutes');
 const adminRoutes = require('./AdminRoutes');
-var cors = require('cors');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-const uri = "mongodb+srv://oussama:oussama@cluster0.qgvvxvt.mongodb.net/Patients";
-//const uri = "mongodb+srv://oussama:oussama@cluster0.qgvvxvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-//const uri = "mongodb+srv://oussama:oussama@cluster0.21sdqm1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const uri = process.env.MONGODB_URI;
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://medouss.vercel.app/', // specify your frontend domain
+}));
+app.use(helmet());
 app.use(express.json());
-app.use('/api/users', userRoutes)
-app.use('/api', visitRoutes)
-app.use('/',  adminRoutes)
+app.use('/api/users', userRoutes);
+app.use('/api', visitRoutes);
+app.use('/', adminRoutes);
 
-mongoose.connect(uri, { })
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Express server listening on port ${PORT}`);
