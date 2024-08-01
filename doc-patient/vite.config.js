7671e5cd9e171feb59bc0c3fcc99198369c3fd2e
Ouss77 <oussamasassour24@gmail.com>
2024-08-01 15:02:18 +0100

    install fontawesome

--- doc-patient/vite.config.js
+++ doc-patient/vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // export default defineConfig({
 //   plugins: [react()],
@@ -10,12 +9,15 @@ import react from '@vitejs/plugin-react'
 //   },
 // });
 
-
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost', // Change this line
+    host: "localhost", // Change this line
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      external: [papaparse],
+    },
   },
 });
